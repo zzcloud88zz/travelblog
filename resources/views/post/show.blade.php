a4dbaa2ac4b066fdde20a64656ff401188c15573
shengrong <shengrong_88@hotmail.com>
2020-10-28 01:32:03 +0800

    Commit Message

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <img src="/storage/{{ $post->image }}" class="w-100">
+            <img src="/{{ $post->image }}" class="w-100">
         </div>
         <div class="col-4">
             <h2>{{$user->name}}</h2>
