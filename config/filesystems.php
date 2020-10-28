de29f91a3b51ff948e064633a0dddfd49bb3cff1
shengrong <shengrong_88@hotmail.com>
2020-10-28 22:16:19 +0800

    Commit Message

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => public_path(),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'profile/',
             'visibility' => 'public',
         ],
 
